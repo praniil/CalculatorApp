f91ef3fd20126ff81475c56ae69ddfe19e03657b
praniil <pparajuli15@gmail.com>
2023-10-27 20:13:10 +0545

    feat: updated its styling

--- src/Components/main.tsx
+++ src/Components/main.tsx
@@ -3,59 +3,28 @@ import { buttonsArray } from "./buttons";
 import { stringify } from "querystring";
 
 const Main = () => {
-  interface screenInterface {
-    sign: string;
-    num: string;
-    res: number;
-  }
-  const [screen, setScreen] = useState<screenInterface>({
-    sign: "",
-    num: "0",
-    res: 0,
-  });
 
-  function handleClick(event: string) {
-    console.log(screen.num);
-    if (/[0-9]/.test(event)) {
-      setScreen((prevScreen) => ({
-        ...prevScreen,
-        num: prevScreen.num === "0" ? event : prevScreen.num + event,
-      }));
-    }
-    if (/[%,+=]/.test(event)) {
-      setScreen((prevScreen) => ({
-        ...prevScreen,
-        sign: prevScreen.sign === "" ? event : prevScreen.sign + event,
-      }));
-    }
-    if (/[+/-/*-]/.test(event)) {
-      setScreen((prevScreen) => ({
-        ...prevScreen,
-        sign: prevScreen.sign === "" ? event : prevScreen.sign + event,
-      }));
-    }
-}
-    return (
-      <div>
-        <div className="w-64 bg-red-400 h-14 m-auto p-2 text-right">
-          {screen.num}{screen.sign}
-        </div>
-        <div className="grid grid-rows-5 grid-cols-4 gap-1 w-64 h-80 m-auto">
-          {buttonsArray.flat().map((buttons, index) => (
-            <button
-              className="p-3 m-3 w-12 bg-red-400 "
-              key={index}
-              value={buttons}
-              onClick={() => handleClick(buttons)}
-            >
-              {" "}
-              {buttons}{" "}
-            </button>
-          ))}
-        </div>
+  const [screen, setScreen] = useState("0");
+  function handleClick(event: string) {}
+
+  return (
+    <div className="m-14">
+      <div className="w-64  h-14 m-auto p-2 text-right border-2">{screen}</div>
+      <div className="grid grid-rows-5 grid-cols-4 gap-1 w-64 h-80 m-auto">
+        {buttonsArray.flat().map((buttons, index) => (
+          <button
+            className=" m-3 w-12 bg-black-400 hover:border-2"
+            key={index}
+            value={buttons}
+            onClick={() => handleClick(buttons)}
+          >
+            {" "}
+            {buttons}{" "}
+          </button>
+        ))}
       </div>
-    );
-  
+    </div>
+  );
 };
 
 export default Main;
