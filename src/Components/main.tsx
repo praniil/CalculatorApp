40d205087bd696df4e5269d5f037c7452eddcc18
praniil <pparajuli15@gmail.com>
2023-10-28 09:26:32 +0545

    feat : added calculation logic

--- src/Components/main.tsx
+++ src/Components/main.tsx
@@ -3,9 +3,62 @@ import { buttonsArray } from "./buttons";
 import { stringify } from "querystring";
 
 const Main = () => {
-
   const [screen, setScreen] = useState("0");
-  function handleClick(event: string) {}
+  const [currentInput, setCurrentInput] = useState("");
+  const [previousInput, setPreviousInput] = useState("");
+  const [operator, setOperator] = useState("");
+  function handleClick(event: string) {
+    if (/[0-9]/.test(event)) {
+      setScreen((prevScreen) => {
+        const newInput = prevScreen === "0" ? event : prevScreen + event;
+        setCurrentInput(newInput);
+        return newInput;
+      });
+    } else if (["+", "-", "/", "*"].includes(event)) {
+      console.log(event);
+      setScreen((prevScreen) => {
+        if (currentInput !== "") {
+          setPreviousInput(currentInput);
+          setOperator(event);
+          const newInput = prevScreen + "" + event + "";
+          setCurrentInput("");
+          return newInput;
+        } else {
+          return prevScreen;
+        }
+      });
+    } else if (event == "C") {
+      setScreen("0");
+      setCurrentInput("0");
+    } else if (event == "=") {
+      if (previousInput && operator && currentInput) {
+        const result = Calculate(previousInput, operator, currentInput);
+        console.log(result)
+        setScreen(result.toString());
+        
+        console.log(screen);
+        setPreviousInput("");
+        setCurrentInput("");
+        setOperator("");
+      }
+    }
+  }
+  function Calculate(number1: string, operator : string , number2: string) {
+    const num1 = parseFloat(number1);
+    const num2 = parseFloat(number2);
+    switch (operator) {
+      case "+" :
+        return num1 + num2;
+      case "*" :
+        return num1 * num2;
+      case "-" :
+        return num1 - num2;
+      case "/" :
+        return num1 / num2;
+      default:
+        return number2;
+    }
+  }
 
   return (
     <div className="m-14">
